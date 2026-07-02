0d0d3fb907c93a3f90e7bff54da7e0ba3e14e253
Arfaoui Firas <xtobi@users.noreply.github.com>
2026-07-02 22:17:36 +0100

    Add button creation and event handling in script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const buttons = [
 
+{
+name:"🏠 Home",
+command:"home"
+},
+
+{
+name:"◀ Back",
+command:"back"
+},
+
+{
+name:"📋 Recent",
+command:"recent"
+},
+
+{
+name:"🔦 Flash",
+command:"flash"
+}
+
+];
+
+let area = document.getElementById("buttons");
+
+buttons.forEach(btn=>{
+
+let b=document.createElement("button");
+
+b.className="btn";
+
+b.innerHTML=btn.name;
+
+b.onclick=()=>{
+
+alert(btn.command);
+
+};
+
+area.appendChild(b);
+
+});
